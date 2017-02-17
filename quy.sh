ab48cf65f2eed0798b5e23b06fca10c7c256d761
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-02-17 14:49:44 +0800

    Create quy.sh

---
+++ quy.sh
@@ -0,0 +1,11 @@
+bamCoverage -b ENCFF226IAA.bam \
+--numberOfProcessors max \
+--normalizeTo1x 2451960000 \
+-bs 1 \
+-o ENCFF226IAA.bw
+
+bamCoverage -b ENCFF632WSK.bam \
+--numberOfProcessors max \
+--normalizeTo1x 2451960000 \
+-bs 1 \
+-o ENCFF632WSK.bw
