bb5f35ceee23173cfbb5fd0d171c81d0a99f1be0
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-03-03 15:43:53 +0800

    Update quy.sh

--- quy.sh
+++ quy.sh
@@ -9,3 +9,7 @@ bamCoverage -b ENCFF632WSK.bam \
 --normalizeTo1x 2451960000 \
 -bs 1 \
 -o ENCFF632WSK.bw
+
+####
+# Muscle
+more muscle-I20170303-072903-0335-50208034-es.clw |perl -pe 's/\n/HOLA/g'|perl -pe 's/\s+/\t/g'|perl -pe 's/HOLA/\n/g'|cut -f2 > meth.cl
