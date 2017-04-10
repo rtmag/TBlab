d095216248581634a58b01200bdf03ddcaf4be36
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-04-10 17:11:06 +0800

    Update 450k_steph.R

--- 450k_steph.R
+++ 450k_steph.R
@@ -24,3 +24,87 @@ comparison <- get.comparisons(WT_VS_TP53_0h_dmr)[1]
 WT_VS_TP53_0h_dmr_table <-get.table(WT_VS_TP53_0h_dmr, comparison, "sites", return.data.frame=TRUE)
 comparison <- get.comparisons(WT_VS_TP53_48h_dmr)[1]
 WT_VS_TP53_48h_dmr_table <-get.table(WT_VS_TP53_48h_dmr, comparison, "sites", return.data.frame=TRUE)
+
+meth.norm<-meth(rnb.set.norm)
+colnames(meth.norm)=rnb.set.norm@pheno$reps
+#
+x=which(abs(WT_VS_TP53_0h_dmr_table$mean.diff)>.25 & WT_VS_TP53_0h_dmr_table$diffmeth.p.adj.fdr<0.05)
+
+colors <- colorRampPalette( (brewer.pal(9, "Blues")) )(255)
+cols=brewer.pal(3, "Set1")
+
+# set the custom distance and clustering functions
+hclustfunc <- function(x) hclust(x, method="complete")
+distfunc <- function(x) dist(x, method="euclidean")
+
+tiff("WT_VS_TP53_0h.tiff",width = 8, height = 7, units = "in", res = 1200)
+y=heatmap.2(meth.norm[x,c(4,8,11,16,20,23)],col=colors, hclustfun=hclustfunc, distfun=distfunc, 
+            scale="none", trace="none",cexCol=0.7,KeyValueName="Methylation Level",
+            dendrogram="both",labRow=FALSE)
+dev.off()
+  hc <- as.hclust( y$rowDendrogram )
+groups=cutree( hc, k=2 )
+  
+  probe.annotation <- rnb.get.annotation("probes450")
+  probe.annotation@unlistData[x,][groups==1]
+  probe.annotation@unlistData[x,][groups==2]
+  
+  gr=probe.annotation@unlistData[x,][groups==1]
+  gr=data.frame(seqnames=seqnames(gr),
+  starts=start(gr)-250,
+  ends=end(gr)+250,
+  names=c(rep(".", length(gr))),
+  scores=c(rep(".", length(gr))),
+  strands=strand(gr))
+ write.table(gr,"WT_VS_TP53_0h_hypoWT.bed",quote=F,row.names=F,col.names=F,sep="\t") 
+  
+  gr=probe.annotation@unlistData[x,][groups==2]
+  gr=data.frame(seqnames=seqnames(gr),
+  starts=start(gr)-250,
+  ends=end(gr)+250,
+  names=c(rep(".", length(gr))),
+  scores=c(rep(".", length(gr))),
+  strands=strand(gr))
+  write.table(gr,"WT_VS_TP53_0h_hyperWT.bed",quote=F,row.names=F,col.names=F,sep="\t") 
+
+  
+#####
+  
+x=which(abs(WT_VS_TP53_48h_dmr_table$mean.diff)>.25 & WT_VS_TP53_48h_dmr_table$diffmeth.p.adj.fdr<0.05)
+
+colors <- colorRampPalette( (brewer.pal(9, "Blues")) )(255)
+cols=brewer.pal(3, "Set1")
+
+# set the custom distance and clustering functions
+hclustfunc <- function(x) hclust(x, method="complete")
+distfunc <- function(x) dist(x, method="euclidean")
+
+tiff("WT_VS_TP53_48h.tiff",width = 8, height = 7, units = "in", res = 1200)
+y=heatmap.2(meth.norm[x,c(4,8,11,16,20,23)],col=colors, hclustfun=hclustfunc, distfun=distfunc, 
+            scale="none", trace="none",cexCol=0.7,KeyValueName="Methylation Level",
+            dendrogram="both",labRow=FALSE)
+dev.off()
+  hc <- as.hclust( y$rowDendrogram )
+groups=cutree( hc, k=2 )
+  
+  probe.annotation <- rnb.get.annotation("probes450")
+  probe.annotation@unlistData[x,][groups==1]
+  probe.annotation@unlistData[x,][groups==2]
+  
+  gr=probe.annotation@unlistData[x,][groups==1 | groups==3]
+  gr=data.frame(seqnames=seqnames(gr),
+  starts=start(gr)-250,
+  ends=end(gr)+250,
+  names=c(rep(".", length(gr))),
+  scores=c(rep(".", length(gr))),
+  strands=strand(gr))
+ write.table(gr,"WT_VS_TP53_48h_hypoWT.bed",quote=F,row.names=F,col.names=F,sep="\t") 
+  
+  gr=probe.annotation@unlistData[x,][groups==2]
+  gr=data.frame(seqnames=seqnames(gr),
+  starts=start(gr)-250,
+  ends=end(gr)+250,
+  names=c(rep(".", length(gr))),
+  scores=c(rep(".", length(gr))),
+  strands=strand(gr))
+  write.table(gr,"WT_VS_TP53_48h_hyperWT.bed",quote=F,row.names=F,col.names=F,sep="\t") 
