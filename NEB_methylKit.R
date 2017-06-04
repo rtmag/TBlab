261711660b25f65ed0c38afee5f4eb96ce40e621
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-06-04 23:08:25 +0800

    Update NEB_methylKit.R

--- NEB_methylKit.R
+++ NEB_methylKit.R
@@ -1,1 +1,30 @@
+library(methylKit)
 
+file.list=list( "HCT116.Decitabine.WGBS.run1.1_bismark_bt2_pe.CpG_report.txt",                
+                "HCT116.Decitabine.WGBS.run2.1_bismark_bt2_pe.CpG_report.txt",
+                "HCT116.control.WGBS.run1.1_bismark_bt2_pe.CpG_report.txt",
+                "HCT116.control.WGBS.run2.1_bismark_bt2_pe.CpG_report.txt" )
+
+
+# read the files to a methylRawList object: myobj
+myobj=methRead(file.list,
+           sample.id=list("decitabine1","decitabine2","control1","control2"),
+           assembly="hg38",
+           treatment=c(1,1,0,0),
+           context="CpG",
+           pipeline="bismarkCytosineReport",
+           header=FALSE,
+           mincov=1)
+
+meth=unite(myobj, destrand=TRUE,mc.cores=40)
+
+#Pooled
+pooled.meth=pool(meth,sample.ids=c("decitabine","control"))
+pooled.meth=filterByCoverage(pooled.meth,lo.count=5)
+pooled.myDiff=calculateDiffMeth(pooled.meth,num.cores=40)
+
+
+# Calculate Differential methylation
+cov.meth=filterByCoverage(meth,lo.count=5)
+cov.myDiff=calculateDiffMeth(cov.meth,num.cores=40)
+#
