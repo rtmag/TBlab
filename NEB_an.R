2f1e8d613bd2baa8ce91aeffa2b926b461b2521e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-06-13 02:45:35 +0200

    Create NEB_an.R

--- NEB_an.R
+++ NEB_an.R
@@ -1,14 +1,22 @@
 
 #1)CpG are in clusters 
-more all_fish.txt|perl -pe 's/^/chr/g'|perl -pe 's/(chr([1-9]|1[0-9]|2[0-2]))\t/$1HOLAMUNDO\t/g'|perl -pe "s/chr23\t/chrXHOLAMUNDO\t/g"|perl -pe 's/chr24\t/chrYHOLAMUNDO\t/g'|grep "HOLAMUNDO"|perl -pe 's/HOLAMUNDO//g' > DMC_fisher.txt
+more all_fish.txt|perl -pe 's/^/chr/g'|perl -pe 's/(chr([1-9]|1[0-9]|2[0-2]))\t/$1HOLAMUNDO\t/g'| \
+perl -pe "s/chr23\t/chrXHOLAMUNDO\t/g"|perl -pe 's/chr24\t/chrYHOLAMUNDO\t/g'|grep "HOLAMUNDO"| \
+perl -pe 's/HOLAMUNDO//g' > DMC_fisher.txt
 # R
 dmc=read.table("DMC_fisher.txt",sep="\t",stringsAsFactors=F)
-
-chr="chr1"
-ix=(dmc[,1]==chr)
-difference=diff(dmc[ix,2])
-position=dmc[ix,2]
-plot(dmc[ix,2],-log10(difference))
+options(scipen=999)
+pdf("1_distance_DMC.pdf")
+chr=c(paste("chr",1:22,sep=""),"chrX","chrY")
+for(i in 1:length(chr)){
+  ix=(dmc[,1]==chr[i])
+  difference=diff(dmc[ix,2])
+  position=dmc[ix,2]
+  position=position[-length(position)]
+  plot(position,-log10(difference),xlab="Chromosome position",ylab="-Log10 Distance between Differentially methylated CpG",main=chr[i])
+  plot(density(log10(difference)),main=chr[i])
+}
+dev.off()
 
 #2)are they in CpG islands?
 #3)Are they demethylated in groups or individual CpGs
