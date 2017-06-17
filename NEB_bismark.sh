2e2a436d0585af3159a7c4e40dd3e5ebed16e585
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-06-17 21:21:04 +0200

    Update NEB_bismark.sh

--- NEB_bismark.sh
+++ NEB_bismark.sh
@@ -26,3 +26,16 @@
 
 ~/myPrograms/Bismark/bismark_methylation_extractor --multicore 62 --gzip --buffer_size 100G --paired-end --ample_memory --comprehensive --cytosine_report --genome_folder /root/resources/HCT116_bs/ HCT116.control.WGBS.run1.1_bismark_bt2_pe.bam
 ~/myPrograms/Bismark/bismark_methylation_extractor --multicore 62 --gzip --buffer_size 100G --paired-end --ample_memory --comprehensive --cytosine_report --genome_folder /root/resources/HCT116_bs/ HCT116.control.WGBS.run2.1_bismark_bt2_pe.bam
+
+###
+
+~/myPrograms/Bismark/bismark --bowtie2 --multicore 64 \
+/root/resources/HCT116_bs/ \
+-1 P007_48h_doxo_1.fq.gz \
+-2 P007_48h_doxo_2.fq.gz
+
+~/myPrograms/Bismark/bismark --bowtie2 --multicore 64 \
+/root/resources/HCT116_bs/ \
+-1 TP53del_48h_doxo_1.fq.gz \
+-2 TP53del_48h_doxo_2.fq.gz
+#
