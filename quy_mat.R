dcb8895c1fcbbd00c9e9d04bf381237d758d950e
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-07-10 14:44:31 +0200

    Update quy_mat.R

--- quy_mat.R
+++ quy_mat.R
@@ -1,1 +1,4 @@
 
+read.table("fisher_neb_diff.mat", colClasses = c(rep("NULL", 1), rep("integer", 4000),rep("NULL",20006), header = TRUE)
+           
+x=read.table("fisher_neb_diff.mat", row.names=1, header = TRUE)
