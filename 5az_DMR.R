f08de0654d870f2a320667a4ce39eb79ce3d0155
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-07-12 18:44:14 +0200

    Update 5az_DMR.R

--- 5az_DMR.R
+++ 5az_DMR.R
@@ -1,4 +1,24 @@
 
+myobj=methRead(file.list,
+           sample.id=list("decitabine1","decitabine2","control1","control2"),
+           assembly="hg38",
+           treatment=c(1,1,0,0),
+           context="CpG",
+           pipeline="bismarkCytosineReport",
+           header=FALSE,
+           mincov=1)
+meth=unite(myobj, destrand=TRUE,mc.cores=40)
+
+pooled.meth=pool(meth,sample.ids=c("decitabine","control"))
+pooled.meth.cov=pooled.meth[as.logical(rowSums(getData(pooled.meth)[,c(5,8)]>5)),]
+pooled.myDiff=calculateDiffMeth(pooled.meth.cov,num.cores=40)
+
+
+
+pooledData=getData(pooled.meth.cov)
+fisher_neb=data.frame(pooled.myDiff,decitabine=(pooledData[,6]/pooledData[,5]),control=(pooledData[,9]/pooledData[,8]) ) 
+
+####################################################
 library(methylKit)
 
 file.list=list( "HCT116.Decitabine.WGBS.run1.1_bismark_bt2_pe.CpG_report.txt",                
