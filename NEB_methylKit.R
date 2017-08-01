812b0c02f81f761c759cdbbb5ffa9d385131c8e9
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-08-01 15:07:11 +0200

    Update NEB_methylKit.R

--- NEB_methylKit.R
+++ NEB_methylKit.R
@@ -25,9 +25,21 @@ pooled.myDiff=calculateDiffMeth(pooled.meth,num.cores=40)
 
 
 # Calculate Differential methylation
-cov.myobj=filterByCoverage(myobj,lo.count=3)
+cov.myobj=filterByCoverage(myobj,lo.count=5)
 meth=unite(cov.myobj, destrand=TRUE,mc.cores=40)
 cov.myDiff=calculateDiffMeth(meth,num.cores=40)
+cmda=cov.myDiff[(cov.myDiff$qvalue<0.01 & cov.myDiff$meth.diff<(-30)),]
+cmda=getData(cmda)
+cmda=cmda[as.numeric(cmda[,1])<26,]
+chrNames=gsub("^","chr",cmda[,1],perl=T)
+chrNames=gsub("chr23","chrX",chrNames,perl=T)
+chrNames=gsub("chr24","chrY",chrNames,perl=T)
+chrNames=gsub("chr25","chrM",chrNames,perl=T)
+cmda[,1]=chrNames
+cmda[,3]=cmda[,3]+1
+
+write.table(cmda,"diff_30diff_qval01.bed",quote=F,row.names=F,col.names=F,sep="\t")
+
 #
 
 ###FISHER
