5d2740c048ff041e087acc1554963b773e66f7f2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-08-21 00:00:52 +0200

    Update ayako.sh

--- ayako.sh
+++ ayako.sh
@@ -25,6 +25,14 @@ _overhang100 \
 macs2 callpeak -f BAMPE -g mm --call-summits --nolambda --buffer-size 1000000 -q 0.001 -t ATAC-20k_S1_Aligned.sortedByCoord.out.bam -n ATAC_s1 &
 macs2 callpeak -f BAMPE -g mm --call-summits --nolambda --buffer-size 1000000 -q 0.001 -t ATAC-20k_S2_Aligned.sortedByCoord.out.bam -n ATAC_s2 &
 #
-macs2 callpeak -f BAMPE -g mm --call-summits --nolambda --buffer-size 1000000 -q 0.01 -t ATAC-20k_S1_Aligned.sortedByCoord.out.bam -n ATAC_s1_fdr1 &
-macs2 callpeak -f BAMPE -g mm --call-summits --nolambda --buffer-size 1000000 -q 0.01 -t ATAC-20k_S2_Aligned.sortedByCoord.out.bam -n ATAC_s2_fdr1 &
+macs2 callpeak -f BAMPE -g mm --call-summits --buffer-size 1000000 -q 0.01 -t ATAC-20k_S1_Aligned.sortedByCoord.out.bam -n ATAC_s1_fdr1 &
+macs2 callpeak -f BAMPE -g mm --call-summits --buffer-size 1000000 -q 0.01 -t ATAC-20k_S2_Aligned.sortedByCoord.out.bam -n ATAC_s2_fdr1 &
+#
+macs2 callpeak -f BAMPE -g mm --call-summits --nolambda --buffer-size 1000000 -q 0.0001 -t ATAC-20k_S1_Aligned.sortedByCoord.out.bam -n ATAC_s1 &
+macs2 callpeak -f BAMPE -g mm --call-summits --nolambda --buffer-size 1000000 -q 0.0001 -t ATAC-20k_S2_Aligned.sortedByCoord.out.bam -n ATAC_s2 &
+#
+
+
+macs2 callpeak -f BAMPE -g mm --call-summits --buffer-size 1000000 -q 0.0001 -t ATAC-20k_S1_Aligned.sortedByCoord.out.bam -n ATAC_s1_fdr0001 &
+macs2 callpeak -f BAMPE -g mm --call-summits --buffer-size 1000000 -q 0.0001 -t ATAC-20k_S2_Aligned.sortedByCoord.out.bam -n ATAC_s2_fdr0001 &
 #
