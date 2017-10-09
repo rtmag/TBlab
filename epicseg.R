f383b69d0c6dfa0404538610d81dfbade7869f3a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-10-09 19:11:21 +0800

    Update epicseg.R

--- epicseg.R
+++ epicseg.R
@@ -5,6 +5,6 @@ Rscript ~/myPrograms/epicseg.R getcounts \
                     -m H3K4me1:/root/HCT116_histone_modification_Quy/STAR_alignment/bam/H3K4me1_rmdup_sort.bam \
                     -m H3K4me3:/root/HCT116_histone_modification_Quy/STAR_alignment/bam/H3K4me3_rmdup_sort.bam \
                     -m H3K9me3:/root/HCT116_histone_modification_Quy/STAR_alignment/bam/H3K9me3_rmdup_sort.bam \
-                    -r hg38.bed --binsize 220 \
+                    -r hg38_normal.bed --binsize 220 \
                     -t hct116_counts.txt
                     
