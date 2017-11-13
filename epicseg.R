83ece5b591da65bd27efae7da9fae62d36a3a152
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-11-13 17:44:05 +0800

    Update epicseg.R

--- epicseg.R
+++ epicseg.R
@@ -8,3 +8,28 @@ Rscript ~/myPrograms/epicseg.R getcounts \
                     -r hg38_normal.bed --binsize 220 \
                     -t hct116_counts.txt
                     
+#
+Rscript ~/myPrograms/epicseg.R segment --counts hct116_counts.txt \
+                  --regions hct116_counts_refined_regions.bed \
+                  --annot genes:/root/resources/hg38_genes.bed \
+                  --nstates 5 --outdir ./segmentation/ --prefix hct116_n5_ &
+                  
+Rscript ~/myPrograms/epicseg.R segment --counts hct116_counts.txt \
+                  --regions hct116_counts_refined_regions.bed \
+                  --annot genes:/root/resources/hg38_genes.bed \
+                  --nstates 6 --outdir ./segmentation/ --prefix hct116_n6_ &
+                  
+Rscript ~/myPrograms/epicseg.R segment --counts hct116_counts.txt \
+                  --regions hct116_counts_refined_regions.bed \
+                  --annot genes:/root/resources/hg38_genes.bed \
+                  --nstates 7 --outdir ./segmentation/ --prefix hct116_n7_ &
+                  
+Rscript ~/myPrograms/epicseg.R segment --counts hct116_counts.txt \
+                  --regions hct116_counts_refined_regions.bed \
+                  --nstates 8 --outdir ./segmentation/ --prefix hct116_n8_ &
+                  
+Rscript ~/myPrograms/epicseg.R segment --counts hct116_counts.txt \
+                  --regions hct116_counts_refined_regions.bed \
+                  --annot genes:/root/resources/hg38_genes.bed \
+                  --nstates 9 --outdir ./segmentation/ --prefix hct116_n9_ &
+#
