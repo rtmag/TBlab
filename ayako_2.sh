79c0b4bbc71a81242dc1127150886d42885b2657
Roberto Tirado Magallanes <rtmag4@gmail.com>
2017-11-27 18:20:28 +0800

    Create ayako_2.sh

---
+++ ayako_2.sh
@@ -0,0 +1,78 @@
+~/programs/STAR/bin/Linux_x86_64/STAR --genomeDir /root/star_index/STAR_mm10_nogtf/ \
+_overhang100 \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ayako/ayako_dejavu/RawData/CD41_ATAC1/CD41_ATAC1_SD17090476_HG2JVCCXY_L2_1.fq.gz \
+/root/ayako/ayako_dejavu/RawData/CD41_ATAC1/CD41_ATAC1_SD17090476_HG2JVCCXY_L2_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/bam/
+
+~/programs/STAR/bin/Linux_x86_64/STAR --genomeDir /root/star_index/STAR_mm10_nogtf/ \
+_overhang100 \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ayako/ayako_dejavu/RawData/CD41_ATAC2/CD41_ATAC2_SD17090477_HG2JVCCXY_L2_1.fq.gz \
+/root/ayako/ayako_dejavu/RawData/CD41_ATAC2/CD41_ATAC2_SD17090477_HG2JVCCXY_L2_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/bam/
+
+~/programs/STAR/bin/Linux_x86_64/STAR --genomeDir /root/star_index/STAR_mm10_nogtf/ \
+_overhang100 \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ayako/ayako_dejavu/RawData/CD41_ATAC3/CD41_ATAC3_SD17090478_HG2JVCCXY_L2_1.fq.gz \
+/root/ayako/ayako_dejavu/RawData/CD41_ATAC3/CD41_ATAC3_SD17090478_HG2JVCCXY_L2_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/bam/
+##############################
+##############################
+~/programs/STAR/bin/Linux_x86_64/STAR --genomeDir /root/star_index/STAR_mm10_nogtf/ \
+_overhang100 \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ayako/ayako_dejavu/C401SC17100122/raw_data/T2/T2_SGL00012_HGLVWCCXY_L1_1.fq.gz \
+/root/ayako/ayako_dejavu/C401SC17100122/raw_data/T2/T2_SGL00012_HGLVWCCXY_L1_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/bam/
+
+~/programs/STAR/bin/Linux_x86_64/STAR --genomeDir /root/star_index/STAR_mm10_nogtf/ \
+_overhang100 \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ayako/ayako_dejavu/C401SC17100122/raw_data/T3/T3_SGL00013_HGLVWCCXY_L1_1.fq.gz \
+/root/ayako/ayako_dejavu/C401SC17100122/raw_data/T3/T3_SGL00013_HGLVWCCXY_L1_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/bam/
+
+~/programs/STAR/bin/Linux_x86_64/STAR --genomeDir /root/star_index/STAR_mm10_nogtf/ \
+_overhang100 \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ayako/ayako_dejavu/C401SC17100122/raw_data/T_2/T_2_SGL00014_HGLVWCCXY_L1_1.fq.gz \
+/root/ayako/ayako_dejavu/C401SC17100122/raw_data/T_2/T_2_SGL00014_HGLVWCCXY_L1_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/bam/
+
+
+~/programs/STAR/bin/Linux_x86_64/STAR --genomeDir /root/star_index/STAR_mm10_nogtf/ \
+_overhang100 \
+--readFilesCommand zcat \
+--runThreadN 15 \
+--alignIntronMax 1 \
+--alignEndsType EndToEnd \
+--readFilesIn /root/ayako/ayako_dejavu/C401SC17100122/raw_data/T_3/T_3_SGL00015_HGLVWCCXY_L1_1.fq.gz \
+/root/ayako/ayako_dejavu/C401SC17100122/raw_data/T_3/T_3_SGL00015_HGLVWCCXY_L1_2.fq.gz \
+--outSAMtype BAM SortedByCoordinate \
+--outFileNamePrefix /root/bam/
