8b2f7bf46df9d77c056e0916f690f78f22ee32bd
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-26 18:25:18 +0800

    Update heatmaps.bash

--- h2az/heatmaps.bash
+++ h2az/heatmaps.bash
@@ -1,1 +1,27 @@
 
+###########################################################################################################
+bamCoverage -p max -bs 1 --normalizeUsing CPM --ignoreDuplicates \
+-b /root/quy/ach2az/HL-60_ActD_hiEU/GRCh38.r88.align/after.trimming/Aligned.sortedByCoord.out.bam \
+-o /root/quy/ach2az/HL-60_ActD_acH2AZ.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM --ignoreDuplicates \
+-b /root/quy/ach2az/HL-60_DMSO_hiEU/GRCh38.r88.align/after.trimming/Aligned.sortedByCoord.out.bam \
+-o /root/quy/ach2az/HL-60_DMSO_acH2AZ.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM --ignoreDuplicates \
+-b /root/quy/ach2az/HL-60_DRB_hiEU/GRCh38.r88.align/after.trimming/Aligned.sortedByCoord.out.bam \
+-o /root/quy/ach2az/HL-60_DRB_acH2AZ.bw
+###########################################################################################################
+bamCoverage -p max -bs 1 --normalizeUsing CPM \
+-b /root/quy/h2az/HL-60_ActD_Aligned.sortedByCoord.out.rmdup.bam \
+-o /root/quy/h2az/HL-60_ActD_H2AZ.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM --ignoreDuplicates \
+-b /root/quy/h2az/HL-60_DMSO_Aligned.sortedByCoord.out.rmdup.bam \
+-o /root/quy/h2az/HL-60_DRB_H2AZ.bw
+
+bamCoverage -p max -bs 1 --normalizeUsing CPM --ignoreDuplicates \
+-b /root/quy/h2az/HL-60_DRB_Aligned.sortedByCoord.out.rmdup.bam \
+-o /root/quy/h2az/HL-60_DMSO_H2AZ.bw
+###########################################################################################################
+
