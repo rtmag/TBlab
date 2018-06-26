f9326ce9eaa2576da5dd09e7199d759ed22e3b07
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-26 20:55:47 +0800

    Update FPKM.R

--- h2az/FPKM.R
+++ h2az/FPKM.R
@@ -22,7 +22,17 @@ x_rpkm <- rpkm(x,x$genes$Length)
 
 colnames(x_rpkm)=c("ActD","DMSO","DRB")
 
+write.table(x_rpkm, "rpkm_wt_hl-60_all.txt",sep="\t",col.names=T,row.names=T,quote=F)
 write.csv(x_rpkm, "rpkm_wt_hl-60_all.csv")
+############################################################################################
+tss = read.table("hg38_tss_knownCanonical.bed",sep="\t")
+rpkm = read.table("rpkm_names.txt",sep="\t")
+rpkm = as.character(rpkm[,1])
+tss = tss[tss[,4] %in% rpkm,]
+tss_num = table(tss[,4])
+tss_num = tss_num[tss_num > 1]
+tss_filtered = tss[!(tss[,4] %in% names(tss_num)), ]
+write.table(tss_filtered,"hg38_tss_filteredbyRPKM.bed",quote=F,sep="\t",col.names=T,row.names=T)
 ############################################################################################
 rpkm = x_rpkm
 
