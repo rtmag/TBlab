6c5a90e0fc4613f8ad05bc17f80e29cfd99cf307
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-06-26 20:31:41 +0800

    Update FPKM.R

--- h2az/FPKM.R
+++ h2az/FPKM.R
@@ -2,7 +2,6 @@ library(edgeR)
 library(Rsubread)
 options(scipen=999)
 
-
 data<-featureCounts(c(
 "HL-60_ActD/Aligned.sortedByCoord.out.bam",
 "HL-60_DMSO/Aligned.sortedByCoord.out.bam",
@@ -21,52 +20,11 @@ GTF.attrType="gene_name"
 x <- DGEList(counts=data$counts, genes=data$annotation[,c("GeneID","Length")])
 x_rpkm <- rpkm(x,x$genes$Length)
 
-colnames(x_rpkm)=c("wt1","wt2","wt3","dn1","dn2","dn3")
+colnames(x_rpkm)=c("ActD","DMSO","DRB")
 
-write.csv(x_rpkm, "rpkm_wt_dm_all.csv")
+write.csv(x_rpkm, "rpkm_wt_hl-60_all.csv")
+############################################################################################
+rpkm = x_rpkm
 
+log_rpkm =log2(rpkm+1)
 
-data0<-featureCounts(c(
-"HL-60_ActD/Aligned.sortedByCoord.out.bam",
-"HL-60_DMSO/Aligned.sortedByCoord.out.bam",
-"HL-60_DRB/Aligned.sortedByCoord.out.bam"
-),
-annot.ext="/home/rtm/references/gencode.v28.annotation.gtf",
-isGTFAnnotationFile=TRUE,
-minMQS=4,
-strandSpecific=0,
-isPairedEnd=TRUE,
-autosort=TRUE,
-nthreads=20,
-GTF.attrType="gene_name"
-)
-
-data1<-featureCounts(c(
-"HL-60_ActD/Aligned.sortedByCoord.out.bam",
-"HL-60_DMSO/Aligned.sortedByCoord.out.bam",
-"HL-60_DRB/Aligned.sortedByCoord.out.bam"
-),
-annot.ext="/home/rtm/references/gencode.v28.annotation.gtf",
-isGTFAnnotationFile=TRUE,
-minMQS=4,
-strandSpecific=1,
-isPairedEnd=TRUE,
-autosort=TRUE,
-nthreads=20,
-GTF.attrType="gene_name"
-)
-
-data2<-featureCounts(c(
-"HL-60_ActD/Aligned.sortedByCoord.out.bam",
-"HL-60_DMSO/Aligned.sortedByCoord.out.bam",
-"HL-60_DRB/Aligned.sortedByCoord.out.bam"
-),
-annot.ext="/home/rtm/references/gencode.v28.annotation.gtf",
-isGTFAnnotationFile=TRUE,
-minMQS=4,
-strandSpecific=2,
-isPairedEnd=TRUE,
-autosort=TRUE,
-nthreads=20,
-GTF.attrType="gene_name"
-)
