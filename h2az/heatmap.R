4d2404a2aeee3c06b23c61d81d97c21a94baa67f
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-03 19:12:59 +0800

    Update heatmap.R

--- h2az/heatmap.R
+++ h2az/heatmap.R
@@ -1,6 +1,6 @@
 h2az = read.table(pipe("grep -v -P '#|genes' h2azOnly_20bp_2kb_QNORM.rmat"),sep="\t")
 
-ach2az = read.table(pipe("tail -n +4 ach2azOnly_20bp_2kb_QNORM.rmat"),sep="\t")
+ach2az = read.table(pipe("grep -v -P '#|genes' ach2azOnly_20bp_2kb_CPM.rmat"),sep="\t")
 #########################################################################################################################
 # 95% confidence interval
 h2_DRB=colMeans(h2az)[1:200]
@@ -56,62 +56,83 @@ dev.off()
    
 
 #########################################################################################################################
-# Quantilez
-library(gplots)
-library(ggplot2)
+# ratio
+                   
+h2az_DRB = (h2az[,1:200])-(h2az[,401:600])
+h2az_ACTD= (h2az[,201:400])-(h2az[,401:600])
+                   
+h2_DRB=colMeans(h2az_DRB)
+h2_ACTD=colMeans(h2az_ACTD)
+                   
+h2_DRB_conf=apply(h2az_DRB,2,function(x) t.test(x)$conf.int)
+h2_ACTD_conf=apply(h2az_ACTD,2,function(x) t.test(x)$conf.int)
 
-h2_DRB=colMeans(h2az)[1:200]
-h2_ACTD=colMeans(h2az)[201:400]
-h2_DMSO=colMeans(h2az)[401:600]
+c.min=min(h2_DRB_conf,h2_ACTD_conf)
+c.max=max(h2_DRB_conf,h2_ACTD_conf)
 
-ac_DRB=colMeans(ach2az)[1:200]
-ac_ACTD=colMeans(ach2az)[201:400]
-ac_DMSO=colMeans(ach2az)[401:600]
-
-h2_DRB_conf=apply(h2az[,1:200],2,function(x) summary(x)[c(2,5)])
-h2_ACTD_conf=apply(h2az[,201:400],2,function(x) summary(x)[c(2,5)])
-h2_DMSO_conf=apply(h2az[,401:600],2,function(x) summary(x)[c(2,5)])
-
-ac_DRB_conf=apply(ach2az[,1:200],2,function(x) summary(x)[c(2,5)])
-ac_ACTD_conf=apply(ach2az[,201:400],2,function(x) summary(x)[c(2,5)])
-ac_DMSO_conf=apply(ach2az[,401:600],2,function(x) summary(x)[c(2,5)])
-
-#
-library(graphics)
-library(Cairo)
-
-c.min=min(h2_DRB_conf,h2_ACTD_conf,h2_DMSO_conf)
-c.max=max(h2_DRB_conf,h2_ACTD_conf,h2_DMSO_conf)
-
-pdf("H2AZ_profile_1st_3rd_quantiles.pdf")
+#pdf("h2az_ratio.pdf")
 plot(h2_DRB,type="l",ylim=c(c.min,c.max),xaxt='n',xlab="",ylab="Quantile Normalized H2AZ ChIP-Seq Tags",col='#619CFF',lwd=3)
 Axis(side=1, labels=c("-2 KB","TSS","2 KB"),at=c(1,97,200))
 polygon( c(1:200,rev(c(1:200)) ),c(h2_DRB_conf[1,],rev(h2_DRB_conf[2,])), col = alpha('#619CFF',.3), border = NA)
 lines(h2_ACTD,col="#F8766D",lwd=3)
-polygon( c(1:200,rev(c(1:200)) ),c(h2_ACTD_conf[1,],rev(h2_ACTD_conf[2,])), col = alpha('#F8766D',.3), border = NA)       
-lines(h2_DMSO,col="#00BA38",lwd=3)
-polygon( c(1:200,rev(c(1:200)) ),c(h2_DMSO_conf[1,],rev(h2_DMSO_conf[2,])), col = alpha('#00BA38',.3), border = NA)
-legend("topright", legend=c("DRB","ACTD","DMSO"), fill=c('#619CFF','#F8766D','#00BA38'), bty = "n",cex = 1.3)
-abline(v=97,lty=2)
+polygon( c(1:200,rev(c(1:200)) ),c(h2_ACTD_conf[1,],rev(h2_ACTD_conf[2,])), col = alpha('#F8766D',.3), border = NA) 
+legend("topright", legend=c("DRB/DMSO","ACTD/DMSO"), fill=c('#619CFF','#F8766D'), bty = "n",cex = 1.3)
+                   abline(v=97,lty=2)
+
 dev.off()
+
+##########################
+h2az_DRB = (ach2az[,1:200])-(ach2az[,401:600])
+h2az_ACTD= (ach2az[,201:400])-(ach2az[,401:600])
                    
-c.min=min(ac_DRB_conf,ac_ACTD_conf,ac_DMSO_conf)
-c.max=max(ac_DRB_conf,ac_ACTD_conf,ac_DMSO_conf)
-   
-pdf("acH2AZ_profile_1st_3rd_quantiles.pdf")
-plot(ac_DRB,type="l",ylim=c(c.min,c.max),xaxt='n',xlab="",ylab="CPM Normalized acH2AZ ChIP-Seq Tags",col='#619CFF',lwd=3)
+h2_DRB=colMeans(h2az_DRB)
+h2_ACTD=colMeans(h2az_ACTD)
+                   
+h2_DRB_conf=apply(h2az_DRB,2,function(x) t.test(x)$conf.int)
+h2_ACTD_conf=apply(h2az_ACTD,2,function(x) t.test(x)$conf.int)
+
+c.min=min(h2_DRB_conf,h2_ACTD_conf)
+c.max=max(h2_DRB_conf,h2_ACTD_conf)
+
+pdf("ach2az_ratio.pdf")
+#plot(h2_DRB,type="l",ylim=c(c.min,c.max),xaxt='n',xlab="",ylab="CPM Normalized acH2AZ ChIP-Seq Tags",col='#619CFF',lwd=3)
+lines(h2_DRB,col="#619CFF",lwd=3)
+
 Axis(side=1, labels=c("-2 KB","TSS","2 KB"),at=c(1,97,200))
-polygon( c(1:200,rev(c(1:200)) ),c(ac_DRB_conf[1,],rev(ac_DRB_conf[2,])), col = alpha('#619CFF',.3), border = NA)
-lines(ac_ACTD,col="#F8766D",lwd=3)
-polygon( c(1:200,rev(c(1:200)) ),c(ac_ACTD_conf[1,],rev(ac_ACTD_conf[2,])), col = alpha('#F8766D',.3), border = NA)       
-lines(ac_DMSO,col="#00BA38",lwd=3)
-polygon( c(1:200,rev(c(1:200)) ),c(ac_DMSO_conf[1,],rev(ac_DMSO_conf[2,])), col = alpha('#00BA38',.3), border = NA)
-legend("topright", legend=c("DRB","ACTD","DMSO"), fill=c('#619CFF','#F8766D','#00BA38'), bty = "n",cex = 1.3)
-abline(v=97,lty=2)
+polygon( c(1:200,rev(c(1:200)) ),c(h2_DRB_conf[1,],rev(h2_DRB_conf[2,])), col = alpha('#619CFF',.3), border = NA)
+lines(h2_ACTD,col="#F8766D",lwd=3)
+polygon( c(1:200,rev(c(1:200)) ),c(h2_ACTD_conf[1,],rev(h2_ACTD_conf[2,])), col = alpha('#F8766D',.3), border = NA) 
+legend("topright", legend=c("DRB/DMSO","ACTD/DMSO"), fill=c('#619CFF','#F8766D'), bty = "n",cex = 1.3)
+                   abline(v=97,lty=2)
+
 dev.off()
                    
+      ##########################
+#Read in files
+h2az = read.table(pipe("grep -v -P '#|genes' h2azOnly_20bp_2kb_QNORM.rmat"),sep="\t")
+ach2az = read.table(pipe("grep -v -P '#|genes' ach2azOnly_20bp_2kb_CPM.rmat"),sep="\t")
+
+# 1-200 = DRB
+# 201-400 = ACTD
+# 401-600 = DMSO
+
+svg("scatterplots.svg")
+par(mfrow=c(2,2))
+smoothScatter(asinh(rowSums(h2az[,1:200])),asinh(rowSums(h2az[,401:600]) ),xlab= "DRB",ylab="DMSO",main = "H2AZ",xlim=c(6,13),
+            ylim=c(6,13) )   
+abline(a=0,b=1)
                    
+smoothScatter(asinh(rowSums(ach2az[,1:200])),asinh(rowSums(ach2az[,401:600]) ),xlab= "DRB",ylab="DMSO",main = "acH2AZ",xlim=c(0,7),
+            ylim=c(0,7) )   
+abline(a=0,b=1)
                    
+smoothScatter(asinh(rowSums(h2az[,201:400])),asinh(rowSums(h2az[,401:600]) ),xlab= "ACTD",ylab="DMSO",main = "H2AZ",xlim=c(6,13),
+            ylim=c(6,13) )   
+abline(a=0,b=1)
                    
+smoothScatter(asinh(rowSums(ach2az[,201:400])),asinh(rowSums(ach2az[,401:600]) ),xlab= "ACTD",ylab="DMSO",main = "acH2AZ",xlim=c(0,7),
+            ylim=c(0,7) )   
+abline(a=0,b=1)
+dev.off()
                    
                    
