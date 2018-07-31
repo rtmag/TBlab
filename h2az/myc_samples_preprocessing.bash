9e6236f16b271f4a3b9fef1afacfc2482c505b7a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-31 18:43:41 +0800

    Create myc_samples_preprocessing.bash

---
+++ h2az/myc_samples_preprocessing.bash
@@ -0,0 +1,9 @@
+java -jar /home/rtm/myprograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+METRICS_FILE=/home/rtm/Desktop/myc_dan/NTC.mfile \
+INPUT=/home/rtm/Desktop/myc_dan/NTC_H2Az_A004_S1/Aligned.sortedByCoord.out.bam \
+OUTPUT=/home/rtm/Desktop/myc_dan/NTC_rmdup.bam &
+
+java -jar /home/rtm/myprograms/picard/build/libs/picard.jar MarkDuplicates REMOVE_DUPLICATES=true \
+METRICS_FILE=/home/rtm/Desktop/myc_dan/siMyc.mfile \
+INPUT=/home/rtm/Desktop/myc_dan/siMyc_H2Az_A006_S3/Aligned.sortedByCoord.out.bam \
+OUTPUT=/home/rtm/Desktop/myc_dan/siMyc_rmdup.bam &
