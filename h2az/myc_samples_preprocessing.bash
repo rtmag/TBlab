f54da7b015281661e730d37088b6d4d92a984f88
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-07-31 23:02:44 +0800

    Update myc_samples_preprocessing.bash

--- h2az/myc_samples_preprocessing.bash
+++ h2az/myc_samples_preprocessing.bash
@@ -8,10 +8,9 @@ METRICS_FILE=/home/rtm/Desktop/myc_dan/siMyc.mfile \
 INPUT=/home/rtm/Desktop/myc_dan/siMyc_H2Az_A006_S3/Aligned.sortedByCoord.out.bam \
 OUTPUT=/home/rtm/Desktop/myc_dan/siMyc_rmdup.bam &
 
-python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos --paired 1 \
--o /home/rtm/Desktop/myc_dan/NTC_res /home/rtm/Desktop/myc_dan/NTC &> /home/rtm/Desktop/myc_dan/NTC.log
 
-python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos --paired 1 \
--o /home/rtm/Desktop/myc_dan/siMyc_res /home/rtm/Desktop/myc_dan/siMyc &> /home/rtm/Desktop/myc_dan/siMyc.log
+python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos --paired 1 -o NTC_res NTC &> NTC.log
+
+python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos --paired 1 -o siMyc_res siMyc &> siMyc.log
 
 ################################################################################################################
