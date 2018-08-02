cb149a09d92357be1b1757828344a47441080da2
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-08-02 14:36:55 +0800

    Update myc_samples_preprocessing.bash

--- h2az/myc_samples_preprocessing.bash
+++ h2az/myc_samples_preprocessing.bash
@@ -15,5 +15,8 @@ python /home/rtm/myprograms/danpos-2.2.2/danpos.py dpos --paired 1 -o siMyc_res 
 
 python /home/rtm/myprograms/danpos-2.2.2/danpos.py wiq --buffer_size 70 hg38.chrom.sizes_noCHR NTC.smooth.wig:siMyc.smooth.wig &> norm.log
 ################################################################################################################
-~/myprograms/kentUtils/bin/linux.x86_64/wigToBigWig NTC.smooth.qnor.wig /home/rtm/references/hg38.chrom.sizes NTC.smooth.qnor.bw  -clip
-~/myprograms/kentUtils/bin/linux.x86_64/wigToBigWig siMyc.smooth.qnor.wig /home/rtm/references/hg38.chrom.sizes siMyc.smooth.qnor.bw  -clip
+more NTC.smooth.qnor.wig | perl -pe 's/chrom\=/chrom\=chr/g' > NTC.smooth.qnor_chr.wig
+more siMyc.smooth.qnor.wig | perl -pe 's/chrom\=/chrom\=chr/g' > siMyc.smooth.qnor_chr.wig
+
+~/myprograms/kentUtils/bin/linux.x86_64/wigToBigWig NTC.smooth.qnor_chr.wig /home/rtm/references/hg38.chrom.sizes NTC.smooth.qnor.bw  -clip
+~/myprograms/kentUtils/bin/linux.x86_64/wigToBigWig siMyc.smooth.qnor_chr.wig /home/rtm/references/hg38.chrom.sizes siMyc.smooth.qnor.bw  -clip
