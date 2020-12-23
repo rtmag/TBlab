5727d79d1393e78e57b4c52223bf03176987a0e6
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-12-23 23:28:35 +0800

    Update touati_rpkm_dec2020.R

--- touati_rpkm_dec2020.R
+++ touati_rpkm_dec2020.R
@@ -46,5 +46,56 @@ system("samtools index hl60_nuclei_S-phase_1h_clicked.sorted.bam")
 
 system("bamCoverage -p max -bs 1 --normalizeUsing CPM -b hl60_nuclei_S-phase_1h_clicked.sorted.bam -o hl60_nuclei_S-phase_1h_clicked.sorted.bw")
 system("bamCoverage -p max -bs 1 --normalizeUsing CPM -b hl60_nuclei_S-phase_1h.sorted.bam -o hl60_nuclei_S-phase_1h.sorted.bw")
+# counting signal at TSS -2KB; MEAN function to count signal
+system("
+computeMatrix reference-point \
+-S \
+hl60_nuclei_S-phase_1h_clicked.sorted.bw \
+hl60_nuclei_S-phase_1h.sorted.bw \
+-R TSS_gencode.v36.annotation_chrRemoved.bed --referencePoint center \
+--sortRegions keep -bs 2000 -a 0 -b 2000 -p 40 -out hl60_nuclei.mat --outFileNameMatrix hl60_nuclei.rmat --outFileSortedRegions hl60_nuclei_regions.bed
+")
+# counting signal at TSS -2KB; SUM function to count signal
+system("
+computeMatrix reference-point \
+-S \
+hl60_nuclei_S-phase_1h_clicked.sorted.bw \
+hl60_nuclei_S-phase_1h.sorted.bw  --averageTypeBins sum \
+-R TSS_gencode.v36.annotation_chrRemoved.bed --referencePoint center \
+--sortRegions keep -bs 2000 -a 0 -b 2000 -p 40 -out hl60_nuclei_sum.mat --outFileNameMatrix hl60_nuclei_sum.rmat --outFileSortedRegions hl60_nuclei_sum_regions.bed
+")
 
 
+
+#integrating everything
+# mean
+bed<-read.table("hl60_nuclei_regions.bed",sep="\t")
+tss <- read.table(pipe("grep -v '#' hl60_nuclei.rmat|grep -v 'hl60_nuclei_S'"),sep='\t')
+rownames(tss)<-as.character(bed[,4])
+rpkm<-readRDS("hl60_nuclei_rpkm.rds")
+
+rpkm <- rpkm[rownames(rpkm) %in% rownames(tss),]
+tss <- tss[rownames(tss) %in% rownames(rpkm),]
+table(rownames(rpkm) == rownames(tss)) #all match
+
+combined_data <- cbind(rpkm,tss)
+colnames(combined_data)<- c("RPKM_hl60_nuclei_S-phase_1h_clicked","RPKM_hl60_nuclei_S-phase_1h","CPM_mean_hl60_nuclei_S-phase_1h_clicked","CPM_mean_hl60_nuclei_S-phase_1h")
+
+saveRDS(combined_data,"hl60_nuclei_RPKM_meanCPM.rds")
+write.csv(combined_data,"hl60_nuclei_RPKM_meanCPM.csv")
+
+# Sum
+bed<-read.table("hl60_nuclei_sum_regions.bed",sep="\t")
+tss <- read.table(pipe("grep -v '#' hl60_nuclei_sum.rmat|grep -v 'hl60_nuclei_S'"),sep='\t')
+rownames(tss)<-as.character(bed[,4])
+rpkm<-readRDS("hl60_nuclei_rpkm.rds")
+
+rpkm <- rpkm[rownames(rpkm) %in% rownames(tss),]
+tss <- tss[rownames(tss) %in% rownames(rpkm),]
+table(rownames(rpkm) == rownames(tss)) #all match
+
+combined_data <- cbind(rpkm,tss)
+colnames(combined_data)<- c("RPKM_hl60_nuclei_S-phase_1h_clicked","RPKM_hl60_nuclei_S-phase_1h","CPM_sum_hl60_nuclei_S-phase_1h_clicked","CPM_sum_hl60_nuclei_S-phase_1h")
+
+saveRDS(combined_data,"hl60_nuclei_RPKM_sumCPM.rds")
+write.csv(combined_data,"hl60_nuclei_RPKM_sumCPM.csv")
